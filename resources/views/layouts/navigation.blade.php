a816cef3e7175a758f406c76810f1818f567e505
nargyanti <nargyanti@gmail.com>
2022-08-23 15:52:12 +0700

    Create layouts

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg bg-primary navbar-dark bg-opacity-75">
+    <div class="container-fluid">            
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="{{ route('home') }}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('portfolio') }}">Portfolio</a>
+                </li>                 
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('contact') }}">Contact</a>
+                </li>                    
+            </ul>
+        </div>
+    </div>
+</nav>

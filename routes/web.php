e62516ba6df316da4ab320b03abdebf71bc58561
nargyanti <nargyanti@gmail.com>
2022-08-23 14:59:53 +0700

    Add routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn () => view('home'));
+Route::get('/portfolio', fn () => view('contact'));
+Route::get('/contact', fn () => view('contact'));
